171ed47b1f04f1e4acc941a6a87962e78471e167
hidave1999 <105460463+hidave1999@users.noreply.github.com>
2022-05-16 12:48:17 -0700

    Update ProjectRepository.java

--- src/main/java/com/itlizeSession/joole/Repository/ProjectRepository.java
+++ src/main/java/com/itlizeSession/joole/Repository/ProjectRepository.java
@@ -1,17 +1,10 @@
 package com.itlizeSession.joole.Repository;
 
-import com.itlizeSession.joole.Entity.ProductType;
-import com.itlizeSession.joole.Entity.Project;
-
+import com.itlizeSession.joole.Entity.ProjectProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by yang shu
- * 2018-03-11 21:38
- */
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-
+public interface ProjectProductRepository extends JpaRepository<ProjectProduct, Integer> {
 }
